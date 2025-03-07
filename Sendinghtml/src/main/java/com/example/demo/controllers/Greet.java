a8e29a3946abfd2e44c548d0f74ec57aa9321e5f
fathimamarsheenakp <fathimamarsheena@gmail.com>
2025-03-07 20:28:29 +0530

    Updated Sendinghtml

--- Sendinghtml/src/main/java/com/example/demo/controllers/Greet.java
+++ Sendinghtml/src/main/java/com/example/demo/controllers/Greet.java
@@ -1,13 +1,17 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Greet {
 
 	@RequestMapping("/greet")
-	public String greet() {
+	public String greet(@RequestParam String username, Model model) {
+		String messageValue = "Hello " + username;
+		model.addAttribute("message", messageValue);
 		return "home";
 	}
 }
