79b2b532f7f7e5f1178fb8b2491df6592ac0743f
fathimamarsheenakp <fathimamarsheena@gmail.com>
2025-03-07 20:02:54 +0530

    Updated FirstWebApp

--- FirstWebApp/src/main/java/com/example/demo/controller/Greet.java
+++ FirstWebApp/src/main/java/com/example/demo/controller/Greet.java
@@ -1,15 +1,34 @@
 package com.example.demo.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import jakarta.websocket.server.PathParam;
 
 @Controller
 public class Greet {
 	
 	@RequestMapping("/greet")
 	@ResponseBody
-	public String greetUser() {
-		return "HELLO WORLD";
+	public String greetUser(@RequestParam("name") String username) {
+		return "HELLO " + username;
+	}
+	
+	@RequestMapping("/data/{name}")
+	@ResponseBody
+	public String getData(@PathVariable String name) {
+		return "Hello " + name;
+	}
+	
+	@RequestMapping("/display")
+	@ResponseBody
+	public String displayData(@RequestBody Map<String, String> input) {
+		return "Hello " + input.get("username");
 	}
 }
