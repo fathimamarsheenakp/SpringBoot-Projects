d1dedb2b0e3bee2ceaad1156c6b369a378afc987
fathimamarsheenakp <fathimamarsheena@gmail.com>
2025-03-08 11:21:14 +0530

    Added DemoApp

---
+++ DemoApp/src/main/java/com/example/demo/controller/User.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class User {
+	
+	@RequestMapping("/user/{year}")
+	public String getUser(@PathVariable String year, Model model) {
+		String message = Integer.parseInt(year) + 10 + "";
+		model.addAttribute("message", message);
+		return "home";
+	}
+}
